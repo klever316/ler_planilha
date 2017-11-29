ebb9f54efe50157a97d8c0ece9e97e369623ba2a
Filipe Estanieski Botti <04321613337@fcbnudigu153592.tj.ce.gov.br>
2017-11-29 18:11:20 -0300

    layout-search

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module Enterpriseape
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.to_prepare do
+      # Configure single controller layout
+      Devise::SessionsController.layout "login"
+    end
   end
 end
