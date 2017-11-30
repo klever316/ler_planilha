4d8d68035686b7d2d57efb28c75078db7140869b
Filipe Estanieski Botti <04321613337@fcbnudigu153592.tj.ce.gov.br>
2017-11-30 14:54:04 -0300

    add tipo parte to results

--- app/services/scpu-service.rb
+++ app/services/scpu-service.rb
@@ -26,10 +26,15 @@ class SCPUService
             item["dataDistribuicao"] = Time.at(item["dataDistribuicao"].to_f / 1000).strftime("%m/%d/%Y %T")
           end
 
+          if item["partes"].length > 0
+            p = item["partes"].select { |parte| parte["nome"].downcase == nome_parte.downcase }.first            
+            item["tipoParte"] = p["tipoParte"]
+          end
+
           if(poloFiltro == "Todos")
             @@processos << item
           else
-            if(item["partes"].any? { |parte| parte["nome"].downcase == nome_parte.downcase && parte["tipoPolo"].downcase == poloFiltro.downcase })
+            if(item["partes"].any? { |parte| parte["nome"].downcase == nome_parte.downcase && parte["tipoPolo"].downcase == poloFiltro.downcase })              
               @@processos << item              
             end
           end
